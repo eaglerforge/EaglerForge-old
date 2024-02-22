ecf04111873e1eeef93b8580241fa23dd31447a9
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-02-22 16:48:02 +0330

    i forgor :skull:

--- sources/setup/workspace_template/javascript/ModLoader.js
+++ sources/setup/workspace_template/javascript/ModLoader.js
@@ -127,5 +127,8 @@ function loadLoader() {
     console.log(
       "EaglerML: Starting to load " + ModsArr.length + " Mods..."
     );
+    window.returntotalloadedmods = function returntotalloadedmods(){
+      return totalLoaded
+    }
   };
 }
